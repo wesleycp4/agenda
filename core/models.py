3fa2fa0e0d14a8d3e142f27f8a2a57230e801c67
Wesley Camargos <wesleycp4@gmail.com>
2021-10-24 17:27:21 -0300

    Criada tabela de Eventos.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(verbose_name='Título do Evento',max_length=100)
+    descricao = models.TextField(verbose_name='Descrição do Evento',blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(verbose_name='Data de Criação do Evento',auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
